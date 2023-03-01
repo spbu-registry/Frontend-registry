ca4fa434eca5d1e194cd8c541c29f6501d001d9d
DanilNaum <90374685+DanilNaum@users.noreply.github.com>
2023-03-01 11:04:41 +0300

    Разрешен конфликт версий

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      
+      <FistInfo/>
+      <ProjectCard/>
     </div>
   );
 }
