eb0829fa665139a13e6ec4d6c5f0b39b44e01958
Denis Laluev <113942822+AngW1SH@users.noreply.github.com>
2023-04-05 19:41:52 +0300

    Поправил типы в соответствии с информацией от бекенда

--- types/types.ts
+++ types/types.ts
@@ -11,66 +11,71 @@ export interface ProjectCardInList {
 по двум имеющимся проектам, которых возвращает /data/projects
 */
 export interface IAPIProject {
-  projectId: number;
-  name: string;
-  description: string;
-  requirements: any[];
-  client: IAPIClient;
-  clinics: IAPIClinic[];
-  tags: IAPITag[];
-  scientificSupervisor: string;
+  projectId?: number;
+  name?: string;
+  description?: string;
+  requirements?: any[];
+  client?: IAPIClient;
+  students?: IAPIStudent[];
+  clinics?: IAPIClinic[];
+  tags?: IAPITag[];
+  scientificSupervisor?: string;
   resultLink?: string;
-  commits: any[];
-  links: any[];
-  requirementsForPerformers: any[];
-  workFormat: string;
+  commits?: any[];
+  links?: any[];
+  requirementsForPerformers?: any[];
+  workFormat?: IAPIWorkFormat;
   start?: string;
   end?: string;
-  maxStudents: number;
+  maxStudents?: number;
 }
 
 export interface IAPIClient {
-  clientId: number;
-  name: string;
-  email: string;
+  clientId?: number;
+  name?: string;
+  email?: string;
   link?: string;
-  orgName: string;
-  phone: string;
-  students: IAPIStudent[];
-  faculty: IAPIFaculty;
+  orgName?: string;
+  phone?: string;
 }
 
 export interface IAPIStudent {
-  studentId: number;
-  name: string;
-  sex: string;
-  degree: string;
-  email: string;
-  grade: number;
-  educationalProgram: IAPIEducationalProgram;
+  studentId?: number;
+  name?: string;
+  sex?: string;
+  degree?: string;
+  email?: string;
+  grade?: number;
+  educationalProgram?: IAPIEducationalProgram;
 }
 
 export interface IAPIEducationalProgram {
-  educationalProgramId: number;
-  code: string;
-  name: string;
-  faculty: IAPIFaculty;
+  educationalProgramId?: number;
+  code?: string;
+  name?: string;
+  faculty?: IAPIFaculty;
 }
 
 export interface IAPIFaculty {
-  facultyId: number;
-  link: string;
-  name: string;
+  facultyId?: number;
+  link?: string;
+  name?: string;
 }
 
 export interface IAPIClinic {
-  clinicId: number;
+  clinicId?: number;
   link?: string;
-  name: string;
-  faculty: IAPIFaculty;
+  name?: string;
+  faculty?: IAPIFaculty;
 }
 
 export interface IAPITag {
-  tagId: number;
-  name: string;
+  tagId?: number;
+  name?: string;
+}
+
+export enum IAPIWorkFormat {
+  FULL_TIME = "FULL_TIME",
+  DISTANT = "DISTANT",
+  HYBRID = "HYBRID",
 }
