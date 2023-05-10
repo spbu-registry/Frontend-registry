db1523382e7f49e3e62772d56e36170a24903244
KordyD <84672403+KordyD@users.noreply.github.com>
2023-05-10 14:34:12 +0300

    Добавил изменение цвета футера

--- modules/Footer/container/index.tsx
+++ modules/Footer/container/index.tsx
@@ -1,24 +1,35 @@
-import React, { FC } from "react";
-import styles from "./Footer.module.sass";
+import React, { FC } from 'react';
+import styles from './Footer.module.scss';
 // import SvgSelector from "./SvgSelector";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faYoutube } from "@fortawesome/free-brands-svg-icons";
-import { faVk } from "@fortawesome/free-brands-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faVk } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-interface FooterProps {}
+interface FooterProps {
+  footerColor?: string;
+}
 
-const Footer: FC<FooterProps> = () => {
+const Footer: FC<FooterProps> = ({ footerColor = 'opal' }) => {
   return (
-    <div className={styles.footer}>
-      <a href="https://github.com">
-        <FontAwesomeIcon icon={faGithub} className={styles.icon} />
+    <div className={[styles.footer, styles[`${footerColor}`]].join(' ')}>
+      <a href='https://github.com'>
+        <FontAwesomeIcon
+          icon={faGithub}
+          className={[styles.icon, styles[`${footerColor}`]].join(' ')}
+        />
       </a>
-      <a href="https://vk.com">
-        <FontAwesomeIcon icon={faVk} className={styles.icon} />
+      <a href='https://vk.com'>
+        <FontAwesomeIcon
+          icon={faVk}
+          className={[styles.icon, styles[`${footerColor}`]].join(' ')}
+        />
       </a>
-      <a href="https://youtube.com">
-        <FontAwesomeIcon icon={faYoutube} className={styles.icon} />
+      <a href='https://youtube.com'>
+        <FontAwesomeIcon
+          icon={faYoutube}
+          className={[styles.icon, styles[`${footerColor}`]].join(' ')}
+        />
       </a>
     </div>
   );
