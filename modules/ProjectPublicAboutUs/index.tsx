4b594e556bcd8207b81db5478f04fca42c0dc0ab
Denis Laluev <113942822+AngW1SH@users.noreply.github.com>
2023-05-10 15:01:06 +0300

    Правки по компоненту с результатами/студентами/датами проекта Длинные ссылки выезжали за пределы контейнера и обрубать их так, чтобы выглядело красиво, не получилось. Немного ещё поигрался с размерами шрифтов в блоке со студентами, чтобы выглядело не так однообразно

--- modules/ProjectPublicAboutUs/index.tsx
+++ modules/ProjectPublicAboutUs/index.tsx
@@ -1,63 +1,72 @@
-import React, { FC } from 'react';
-import styles from './ProjectPublicAboutUs.module.scss';
-import { DateBlock } from '../ProjectDateBlock';
+import React, { FC } from "react";
+import styles from "./ProjectPublicAboutUs.module.scss";
+import { DateBlock } from "../ProjectDateBlock";
 
 interface ProjectPublicAboutUsProps {}
 
 const ProjectPublicAboutUs: FC<ProjectPublicAboutUsProps> = (props) => {
   return (
-    <div className={styles['aboutus']}>
-      <div className={styles['result']}>
-        <table>
-          <thead>
-            <tr>
-              <th>Результат</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Гитхаб проекта - {}</td>
-            </tr>
-            <tr>
-              <td>Рабочий файл проекта - {}</td>
-            </tr>
-            <tr>
-              <td>Статья об этом проекте {}</td>
-            </tr>
-          </tbody>
-        </table>
+    <div className={styles["aboutus"]}>
+      <div className={styles["result"]}>
+        <div className={styles["result__inner"]}>
+          <h2 className={styles["aboutus__title"]}>Результат</h2>
+          <ul className={styles["result__list"]}>
+            <li>
+              <button>
+                <a href="https://github.com/spbu-registry/Frontend-registry">
+                  Гитхаб проекта
+                </a>
+              </button>
+            </li>
+            <li>
+              <button>
+                <a href="https://github.com/spbu-registry/Frontend-registry">
+                  Рабочий файл проекта
+                </a>
+              </button>
+            </li>
+            <li>
+              <button>
+                <a href="https://github.com/spbu-registry/Frontend-registry">
+                  Статья об этом проекте
+                </a>
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className={styles['roles']}>
-        <table>
-          <thead>
-            <tr>
-              <th>Студенты и их роли</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Продакт-менеджер: {}</td>
-            </tr>
-            <tr>
-              <td>Бэкенд-азработчик: {}</td>
-            </tr>
-            <tr>
-              <td>Фронтэнд-разработичк: {}</td>
-            </tr>
-            <tr>
-              <td>Тестировщик: {}</td>
-            </tr>
-            <tr>
-              <td> Менеджер: {}</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className={styles["roles"]}>
+        <div className={styles["roles__inner"]}>
+          <h2 className={styles["aboutus__title"]}>Студенты и их роли</h2>
+        </div>
+        <ul className={styles["roles__list"]}>
+          <li>
+            <h3>Продакт-менеджер:</h3>
+            <p>Соколова Вероника Сергеевна</p>
+          </li>
+          <li>
+            <h3>Бэкенд-азработчик:</h3>
+            <p>Смирнова Варвара Ильинична</p>
+          </li>
+          <li>
+            <h3>Фронтэнд-разработичк:</h3>
+            <p>Никитин Марк Макарович</p>
+          </li>
+          <li>
+            <h3>Тестировщик:</h3>
+            <p>Золотарева Яна Александровна</p>
+          </li>
+          <li>
+            <h3>Менеджер:</h3>
+            <p>Давыдова Надежда Игоревна</p>
+          </li>
+        </ul>
       </div>
       <DateBlock
-        dateOfCreate='11.10.2011'
-        dateOfRegister='11.10.2011 - 21.10.2012'
-        deadline='11.10.2011 - 21.10.2012'
-        status='открыт'
+        dateOfCreate="11.10.2011"
+        dateOfRegister="11.10.2011 - 21.10.2012"
+        deadline="11.10.2011 - 21.10.2012"
+        status="открыт"
       />
       <button>Подать заявку</button>
     </div>
