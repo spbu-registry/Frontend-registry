211eadbe2304f5d9b0837d89804a44d0cba8ebcc
KordyD <84672403+KordyD@users.noreply.github.com>
2023-05-21 18:37:58 +0300

    Адаптивная версия страницы презентаций команд в админке

--- modules/AdminEditProject/components/LinksToPresentations/index.tsx
+++ modules/AdminEditProject/components/LinksToPresentations/index.tsx
@@ -2,37 +2,50 @@ import styles from './index.module.scss';
 import { closeSVG, docSVG, mapOverMap } from './components';
 
 interface LinksToPresentationsProps {
-    linksAndLabels : Map<string, string>
-    removeLink : (key : string) => void
+  linksAndLabels: Map<string, string>;
+  removeLink: (key: string) => void;
 }
 
-export function LinksToPresentations ({linksAndLabels, removeLink} : LinksToPresentationsProps) {
+export function LinksToPresentations({
+  linksAndLabels,
+  removeLink,
+}: LinksToPresentationsProps) {
+  if (linksAndLabels.size === 0) return <div></div>;
 
-    if (linksAndLabels.size === 0) return <div></div>
-
-    return (<>
-    <div className={styles.BigLabel + ' ' + styles.Row}>
+  return (
+    <>
+      <div className={styles.BigLabel + ' ' + styles.Row}>
         {docSVG(styles.SVG)}
         Команды
-    </div>
-    <div
-    className={styles.AllLinks}
-    style={{'--options' : Math.floor(linksAndLabels.size / 2)} as React.CSSProperties}>
-        {
-            mapOverMap(linksAndLabels, (label, link) => {
-                return <div key={label} className={styles.LinkBox}>
-                    <label id={label + link} className={styles.Row}>{label}</label>
-                    <div className={styles.Row}>
-                        {closeSVG(styles.SVG, () => removeLink(label))}
-                        <a
-                        className={styles.Link} 
-                        aria-labelledby={label + link} 
-                        href={link}>презентация команды</a>
-                    </div>
-                </div>
-            })
+      </div>
+      <div
+        className={styles.AllLinks}
+        style={
+          {
+            '--options': Math.floor(linksAndLabels.size / 2),
+          } as React.CSSProperties
         }
-
-    </div>
+      >
+        {mapOverMap(linksAndLabels, (label, link) => {
+          return (
+            <div key={label} className={styles.LinkBox}>
+              <label id={label + link} className={styles.Row}>
+                {label}
+              </label>
+              <div className={styles.Row}>
+                {closeSVG(styles.SVG, () => removeLink(label))}
+                <a
+                  className={styles.Link}
+                  aria-labelledby={label + link}
+                  href={link}
+                >
+                  презентация команды
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
-)}
+  );
+}
