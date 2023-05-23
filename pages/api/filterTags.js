d648a7e8413c3b2617f6e8330c935fafb08f3a85
Denis Laluev <113942822+AngW1SH@users.noreply.github.com>
2023-05-23 23:47:44 +0300

    поправил url

--- pages/api/filterTags.js
+++ pages/api/filterTags.js
@@ -1,18 +1,19 @@
 import { tags } from "./data/tags";
 
 export default function handler(req, res) {
+  const url = new URL(
+    "http://217.197.0.155/projects" + req.url,
+    req.headers.host
+  );
+  const searchParams = url.searchParams;
 
-    const url = new URL('http://localhost:3000/projects' + req.url, req.headers.host);
-    const searchParams = url.searchParams;
-
-    const ans = [];
-    if (searchParams.get('q') !== '') {
-        for (const tag of tags) {
-            if (tag.toLowerCase().includes(searchParams.get('q').toLowerCase()))
-                ans.push(tag);
-        }
+  const ans = [];
+  if (searchParams.get("q") !== "") {
+    for (const tag of tags) {
+      if (tag.toLowerCase().includes(searchParams.get("q").toLowerCase()))
+        ans.push(tag);
     }
-    
+  }
 
-    res.status(200).json(ans);
+  res.status(200).json(ans);
 }
