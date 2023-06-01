0cb346294bd7f496ec1a0f3ef5176e55658f1b21
Roman Ivashkin <st086961@student.spbu.ru>
2023-06-01 22:55:05 +0300

    Transormed commit data

--- modules/Metrics/components/CommitsPerWeek/index.tsx
+++ modules/Metrics/components/CommitsPerWeek/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useContext, useState } from "react";
+import React, { FC, useContext, useState, useMemo } from "react";
 import { BarChart, Flex, Title, Toggle, ToggleItem, Icon } from "@tremor/react";
 import { InformationCircleIcon } from "@heroicons/react/outline";
 import { DatesContext } from "../../context/dates";
@@ -9,32 +9,73 @@ interface CommitsPerWeekProps {
   commits: IAPICommit[];
 }
 
+interface StringNumberPairs {
+  [key : string] : number
+}
+
 const CommitsPerWeek: FC<CommitsPerWeekProps> = ({ commits }) => {
-  const students = commits
-    .map((commit) => commit.author_login)
-    .filter((name, index, currentVal) => currentVal.indexOf(name) === index);
 
-  const [selectedKpi, setSelectedKpi] = useState("Все");
+  console.log(commits)
 
-  const { dates, setDates } = useContext(DatesContext);
+  const { dates } = useContext(DatesContext);
 
-  const dataByDate = () => {
+  const students = useMemo(() => {
+
+    return Array.from(new Set<string>(
+      commits.map((commit) => commit.author_login)
+    ))
+
+  }, [commits])
+
+  const filteredCommits = useMemo(() => {
     if (!dates || !dates[0] || !dates[1]) return commits;
     return commits.filter(
       (commit) =>
         new Date(commit.created_at) > (dates[0] as Date) &&
         new Date(commit.created_at) < (dates[1] as Date)
     );
-  };
+  }, [commits, dates])
 
-  const formatData = (commits: IAPICommit[]) => {
-    return commits.map((commit) => {
+  const contributionArray = useMemo(() => {
+
+    const contributionByDay = new Map<string, StringNumberPairs>();
+
+    filteredCommits.forEach((commit) => {
+
+      // Add object on that day if it doesn't exist
+      if (!contributionByDay.has(commit.created_at)) {
+        contributionByDay.set(commit.created_at, {})
+        const day = (contributionByDay.get(commit.created_at) as StringNumberPairs)
+
+        // Add contributors
+        students.forEach((student) => {
+          day[student] = 0
+        })
+      }
+
+      const day = (contributionByDay.get(commit.created_at) as StringNumberPairs)
+      day[commit.author_login] += 1
+    })
+
+    const allDates = Array.from(new Set(contributionByDay.keys()))
+    allDates.sort((a, b) => new Date(b).getTime() - new Date(a).getTime())
+
+    return allDates.map((date) => {
+
+      const cDate = new Date(date)
+
       return {
-        date: commit.created_at,
-        [commit.author_login]: 1,
-      };
-    });
-  };
+        date : `${cDate.getDay()}.${cDate.getMonth()}.${cDate.getFullYear()}`,
+        ...contributionByDay.get(date)
+      }
+    })
+    
+  }, [students, filteredCommits])
+
+  console.log(contributionArray)
+
+
+  const [selectedKpi, setSelectedKpi] = useState("Все");
 
   return (
     <>
@@ -70,7 +111,7 @@ const CommitsPerWeek: FC<CommitsPerWeekProps> = ({ commits }) => {
       </div>
       <BarChart
         className="mt-6"
-        data={formatData(dataByDate())}
+        data={contributionArray}
         index="date"
         categories={selectedKpi == "Все" ? students : [selectedKpi]}
         colors={["sky", "violet", "fuchsia", "blue", "cyan"]}
